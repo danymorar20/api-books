af71cf4b788a382901aeacd01cbadc742490af59
danymorar20 <danymorar20@gmail.com>
2025-03-10 01:29:10 -0600

    Setting all in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { BooksModule } from './books/books.module';
+import { BookModule } from '@/book/book.module';
 import { MessageModule } from '@/message/message.module';
 import { AuthModule } from './auth/auth.module';
+import { IdGeneratorProvider } from '@/id-generator/id-generator.provider';
+import { IdGeneratorModule } from '@/id-generator/id-generator.module';
 
 @Module({
   imports: [
-    BooksModule,
+    BookModule,
     MessageModule,
-    AuthModule
+    AuthModule,
+    IdGeneratorModule
   ],
+  providers: [IdGeneratorProvider]
 })
 export class AppModule {}
