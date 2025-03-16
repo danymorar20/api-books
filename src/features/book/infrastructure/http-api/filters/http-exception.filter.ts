dbed1b13d11aef3037d43082bfa0f70418a3d678
danymorar20 <danymorar20@gmail.com>
2025-03-15 19:57:23 -0600

    fixing the book http filter

--- src/features/book/infrastructure/http-api/filters/http-exception.filter.ts
+++ src/features/book/infrastructure/http-api/filters/http-exception.filter.ts
@@ -1,16 +1,10 @@
-import { Response } from "express";
 import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
 import { BooksNotFoundException } from "@/book/domain/exceptions/books-not-found.exception";
+import { HttpBooksNotFoundException } from "@/book/infrastructure/exceptions/http-books-not-found.exception";
 
 @Catch(BooksNotFoundException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: BooksNotFoundException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-
-    response.status(404).json({
-      statusCode: 404,
-      message: exception.message,
-    });
+    throw new HttpBooksNotFoundException();
   }
 }
